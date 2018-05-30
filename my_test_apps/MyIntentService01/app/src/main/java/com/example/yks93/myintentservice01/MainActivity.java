a2e280b4fad377734c969a46d6064a720d26838e
yks93 <keesung0705@gmail.com>
2018-05-30 21:36:43 +0900

    Modified some Files on 2018.05.30

--- my_test_apps/MyIntentService01/app/src/main/java/com/example/yks93/myintentservice01/MainActivity.java
+++ my_test_apps/MyIntentService01/app/src/main/java/com/example/yks93/myintentservice01/MainActivity.java
@@ -2,11 +2,16 @@ package com.example.yks93.myintentservice01;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 
 public class MainActivity extends AppCompatActivity {
 
+
+    private final String TAG = "MainActivity";
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        Log.d(TAG, "onCreate: ");
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
